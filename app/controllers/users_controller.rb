784108e2860f41a13211e2234daf2f7f9cf30292
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-04-21 14:19:26 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -13,13 +14,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
+    @user = User.find(params[:id])
+    @book = Book.all
   end
 
   def update
+    is_matching_login_user
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
@@ -33,6 +39,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
+    end
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
     end
   end
 end
