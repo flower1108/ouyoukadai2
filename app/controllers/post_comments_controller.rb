f0a234db5ddf7fd9b66bfbc7b6f1d43d1a2b30c6
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-05-06 07:40:25 +0000

    [Finish]非同期通信化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,16 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.book_id = @book.id
+    @post_comment.save
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
   end
 
   private
